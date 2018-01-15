3e86b109b11a59a94460dd10ac1c61cfb60260b7
xiaobxia <673806687@qq.com>
2018-01-15 22:32:31 +0800

    xg

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 /**
  * Created by xiaobxia on 2018/1/15.
  */
-const request = require('request');
+const request = require('request-promise');
 const cheerio = require('cheerio');
-const iconv = require('iconv-lite');
+const fs = require('fs-extra');
 
-request(
+const myFund = [
   {
-    method: 'get',
-    url: 'http://fund.eastmoney.com/fundguzhi.html'
+    code: '167301',
+    name: '方正富邦保险主题指数分级',
+    price: 6000
   },
-  function (err, response, body) {
-    if (!err && response.statusCode === 200) {
-      let html = iconv.decode(body, 'utf-8');
-      console.log(html)
-      let $ = cheerio.load(html);
-      let mainData = [];
-    }
+  {
+    code: '001600',
+    name: '天弘高端装备制造指数',
+    price: 3929
   }
-);
+];
+let requestList = [];
+myFund.forEach(function (item) {
+  /**
+   * 天天基金
+   */
+  requestList.push(request({
+    method: 'get',
+    url: `http://fund.eastmoney.com/${item.code}.html?spm=search`,
+    encoding: 'utf-8',
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  }).then(($) => {
+    item.preValue1 = $('#gz_gszzl').text();
+  }).catch(function (err) {
+    console.log(err)
+  }));
+});
+
+Promise.all(requestList).then(() => {
+  logData({myFund});
+});
+
+function logData(fileData) {
+  const fileName = './mock/last.json';
+  fs.ensureFile(fileName).then(() => {
+    fs.writeJson(fileName, fileData, {spaces: 2})
+  });
+}
