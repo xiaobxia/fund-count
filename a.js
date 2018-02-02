51118b4a6b61d491dcc57b6c00c3d29b8448b12a
chenlingjie <chenlingjie@cd121.com>
2018-02-02 17:37:27 +0800

    xg

--- a.js
+++ a.js
@@ -2,3 +2,5 @@
  * Created by xiaobxia on 2018/1/31.
  */
 console.log(0.5<=0.5);
+console.log(`于${new Date().toLocaleString()}执行更新基金单位净值`)
+console.log(new Date())
--- xinlang.js
+++ xinlang.js
@@ -2,19 +2,22 @@
  * Created by xiaobxia on 2018/1/26.
  */
 const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs-extra');
+// 新浪数据太不准
+// request({
+//   method: 'get',
+//   url: `http://hq.sinajs.cn/list=fu_001703`,
+//   encoding: 'utf-8'
+// }).then((body) => {
+//   let temp = body.split(',');
+//   const gsz =  temp[2];
+//   const gszzl = temp[temp.length - 2];
+//   console.log({
+//     gszzl,
+//     gsz
+//   })
+// }).catch(function (err) {
+//   console.log(err)
+// });
 
-request({
-  method: 'get',
-  url: `http://hq.sinajs.cn/list=fu_001703`,
-  encoding: 'utf-8'
-}).then((body) => {
-  let temp = body.split(',');
-  const gsz =  temp[2];
-  const gszzl = temp[temp.length - 2];
-  console.log({
-    gszzl,
-    gsz
-  })
-}).catch(function (err) {
-  console.log(err)
-});
